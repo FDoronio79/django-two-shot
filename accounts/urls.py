37827c46c17773729af546a2ad5108f920e1540e
Filamer Doronio <filamerdoronio@gmail.com>
2022-07-27 17:25:56 -0700

    Feature 9 complete

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path, reverse_lazy
-from django.contrib.auth.views import LoginView
-from django.views.generic.base import RedirectView
+from django.contrib.auth.views import LoginView, LogoutView
 
 
 urlpatterns = [
     path("login/", LoginView.as_view(), name="login"),
-
+    path("logout/", LogoutView.as_view(), name="logout"),
 ]
