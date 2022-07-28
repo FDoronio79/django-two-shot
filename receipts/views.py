7bd8f2a07a58250388ad1b203c08d8a18ae074b0
Filamer Doronio <filamerdoronio@gmail.com>
2022-07-27 17:16:41 -0700

    Feature 8 completed

--- receipts/views.py
+++ receipts/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from receipts.models import ExpenseCategory, Account, Receipt
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def show_receipt(request):
     receipts = Receipt.objects.all()
     context = {"receipts": receipts}
